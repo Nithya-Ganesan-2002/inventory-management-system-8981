de30411a9c90676cf91bffae4e81a05fd53600fc
Kavia AI <production@kavia.ai>
2025-08-20 09:11:23 +0000

    CheckPoint - cgf69759df

---
+++ .init/.linter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/inventory-management-system-8981/frontend
+npm run lint
+ESLINT_EXIT_CODE=$?
+npm run build
+BUILD_EXIT_CODE=$?
+if [ $ESLINT_EXIT_CODE -ne 0 ] || [ $BUILD_EXIT_CODE -ne 0 ]; then
+   exit 1
+fi
+
